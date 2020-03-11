48994e07648fc5bcdc7bd0fae2e539072d67ec19
Grifferonte <lucas.risi93@gmail.com>
2020-03-11 14:04:39 +0100

    ServiceXml

---
+++ src/rosi/ServiceXml.java
@@ -0,0 +1,87 @@
+package rosi;
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import bri.Service;
+
+public class ServiceXml implements Service {
+	private final Socket client;
+	
+	public ServiceXml (Socket socket) {
+		client = socket;
+	}
+	
+	static class Erreur implements ErrorHandler{
+		
+	    public void warning(SAXParseException e) throws SAXException {
+	        System.out.println("Attention : " + e.getMessage());
+	        throw e;
+	    }
+
+	    public void error(SAXParseException e) throws SAXException {
+	        System.out.println("Erreur : " + e.getMessage());
+	        throw e;
+	    }
+
+	    public void fatalError(SAXParseException e) throws SAXException {
+	        System.out.println("Erreur fatal : " + e.getMessage());
+	        throw e;
+	    }
+	}
+	
+   public void run() {
+	   String serveur = "ftp://localhost:2121/";
+	   String chemin = null;
+	   Scanner sc = new Scanner(System.in);
+
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+      try {
+         factory.setValidating(true);
+         
+         DocumentBuilder build = factory.newDocumentBuilder();
+         
+         ErrorHandler Erreur = new Erreur();
+         build.setErrorHandler(Erreur);
+  	   	 System.out.println("Veuillez saisir un mot :");
+  	   	 chemin	= sc.nextLine();
+         File fileXML = new File(serveur + chemin);
+
+         try {
+            Document xml = build.parse(fileXML);
+            System.out.println(xml.getNodeName());
+            Element elem = xml.getDocumentElement();
+            System.out.println(elem.getNodeName());
+         } catch (SAXParseException e){
+        	 e.printStackTrace();
+         }
+         
+      } catch (ParserConfigurationException e) {
+         e.printStackTrace();
+      } catch (SAXException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }      
+
+   }
+   
+	protected void finalize() throws Throwable {
+		 client.close(); 
+	}
+}
+ 
+   
+	
